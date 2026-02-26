c71cdb186f415aa3cf89f2ac3f00ce84fbca23cd
MysterCoder <73602548+dmv2141@users.noreply.github.com>
2026-02-25 21:11:37 -0700

    Starter Code Setup

---
+++ src/scoretrakker/ScoreTrakker.java
@@ -0,0 +1,30 @@
+package scoretrakker;
+
+import java.util.ArrayList;
+
+
+public class ScoreTrakker {
+	
+	private ArrayList<Student> students = new ArrayList<Student>();
+	
+	private void loadDataFile(String fileName) {
+		
+	}
+	
+	private void printInOrder() {
+		
+	}
+	
+	private void processFiles(){
+	//Calls loadDataFile passing in name of correctly formatted file
+	//Calls printInOrderMethod
+		
+	}
+	
+	
+	public static void main(String[] args) {
+		ScoreTrakker s1 = new ScoreTrakker();
+		s1.processFiles();
+	}
+
+}
